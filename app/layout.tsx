3922ee6acbf6dc234c545d1175c0c78c3a842689
gabriel <gsmmendes58@gmail.com>
2025-08-28 22:04:43 -0300

    chore: add shadcn setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
         <h2>Finance</h2>
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return <div className="flex items-center justify-center w-screen">
         <h1 className="text-green-600 p-5"> Teste</h1>
+        <Button> Hello Word</Button>
     </div>;
 }
  
